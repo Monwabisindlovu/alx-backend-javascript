0eb005cd9720728bfbdb306471366cc681aa5ebf
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-08 13:02:14 +0000

    2-arrows.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,12 +1,10 @@
-export default function taskBlock(trueOrFalse) {
-  let task = false; // Use let instead of var for block-scoped variable
-  let task2 = true; // Use let instead of var for block-scoped variable
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  if (trueOrFalse) {
-    task = true; // No need to redeclare, just assign the value
-    task2 = false; // No need to redeclare, just assign the value
-  }
-
-  return [task, task2];
+  // Using arrow function to define addNeighborhood method
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
 }
 
