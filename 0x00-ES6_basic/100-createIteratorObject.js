6a891c0fbf2c0bf4fe34cd48e6f03b3664ae06e5
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-08 18:50:15 +0200

    100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,30 +1,9 @@
 export default function createIteratorObject(report) {
-    const departments = Object.keys(report.allEmployees);
-    let currentDepartmentIndex = 0;
-    let currentEmployeeIndex = 0;
-  
-    const iterator = {
-      next: function () {
-        if (currentDepartmentIndex < departments.length) {
-          const currentDepartment = departments[currentDepartmentIndex];
-          const employees = report.allEmployees[currentDepartment];
-  
-          if (currentEmployeeIndex < employees.length) {
-            const employee = employees[currentEmployeeIndex];
-            currentEmployeeIndex++;
-            return { value: employee, done: false };
-          } else {
-            // Move to the next department
-            currentDepartmentIndex++;
-            currentEmployeeIndex = 0;
-            return this.next(); // Recursive call to handle empty departments
-          }
+    return (function* _() {
+      for (const department of Object.values(report.allEmployees)) {
+        for (const employee of department) {
+          yield employee;
         }
-  
-        // No more departments or employees
-        return { done: true };
-      },
-      [Symbol.iterator]: function () {
-        return this;
-      },
-    };
+      }
+    }());
+  }
