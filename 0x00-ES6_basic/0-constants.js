b771bfe0e2f893bae2ba0b0bc438e36b72e2dbf5
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-08 17:35:06 +0000

    0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,15 @@
-// 0-constants.js
 export function taskFirst() {
-  const task = 'I prefer const when I can.';
-  return task;
+    var task = 'I prefer const when I can.';
+    return task;
+}
+
+export function getLast() {
+    return ' is okay';
+}
+
+export function taskNext() {
+    var combination = 'But sometimes let';
+    combination += getLast();
+
+    return combination;
 }
