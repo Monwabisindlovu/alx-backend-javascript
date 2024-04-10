b4d8123197a78e758b0afe7c2547dd6957f22113
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-10 19:54:16 +0000

    9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,17 +1,12 @@
-function guardrail(mathFunction) {
+export default function guardrail(mathFunction) {
   const queue = [];
-
   try {
-    const result = mathFunction();
-    queue.push(result);
+    const returnedValue = mathFunction();
+    queue.push(returnedValue);
+    queue.push('Guardrail was processed');
   } catch (error) {
-    queue.push(error.message);
-  } finally {
+    queue.push(String(error));
     queue.push('Guardrail was processed');
   }
-
   return queue;
 }
-
-export default guardrail;
-
