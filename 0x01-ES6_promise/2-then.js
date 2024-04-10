d971f09c34ac57700ca82a4773afbc79b0e96467
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-10 15:46:56 +0000

    2-main.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,7 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
+  const successBody = { status: 200, body: 'success' };
   return promise
-    .then((body) => {
-      console.log("Got a response from the API");
-      return { status: 200, body: "success" };
-    })
-    .catch(() => {
-      console.log("Got a response from the API");
-      return { status: null, body: new Error() };
-    });
+    .then(() => successBody)
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
 }
