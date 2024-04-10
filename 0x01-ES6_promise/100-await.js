83f932e858e59466fe7fc520777c8172fc53fa55
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-10 15:49:59 +0000

    100-await.js

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,16 @@
+// 100-await.js
+import { uploadPhoto, createUser } from './utils.js';
+
+async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch (error) {
+    console.error(error);
+    return { photo: null, user: null };
+  }
+}
+
+export default asyncUploadUser;
+
