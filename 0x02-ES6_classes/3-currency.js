afdfb63cc68e29adc6946ca771eb89327eb622cc
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-11 10:50:05 +0000

    3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,38 +1,49 @@
-class Currency {
+// 3-currency.js
+export default class Currency {
   constructor(code, name) {
-    this._code = code;
-    this._name = name;
+    // Verify the types of attributes during object creation
+    if (typeof code === 'string') {
+      // Store attributes in underscore attribute versions
+      this._code = code;
+    } else {
+      throw TypeError('Code must be a string');
+    }
+    if (typeof name === 'string') {
+      // Store attributes in underscore attribute versions
+      this._name = name;
+    } else {
+      throw TypeError('Name must be a string');
+    }
   }
 
-  // Getters
+  // Getter and setter for the 'code' attribute
   get code() {
     return this._code;
   }
 
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      this._code = newCode;
+    } else {
+      throw TypeError('Code must be a string');
+    }
+  }
+
+  // Getter and setter for the 'name' attribute
   get name() {
     return this._name;
   }
 
-  // Setters
-  set code(newCode) {
-    if (typeof newCode !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-    this._code = newCode;
-  }
-
   set name(newName) {
-    if (typeof newName !== 'string') {
-      throw new TypeError('Name must be a string');
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw TypeError('Name must be a string');
     }
-    this._name = newName;
   }
 
-  // Method to display full currency
+  // Method to display the full currency in the specified format
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
 }
-
-export default Currency;
-
