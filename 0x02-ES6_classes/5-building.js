1a539a690d24ef7bab79cd3f1183e9d3eb984904
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-11 10:56:53 +0000

    6-sky_high.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+// 5-building.js
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
