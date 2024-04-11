a09078a7891ab89e717354b14c4c6d3f93af39fe
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-11 11:44:05 +0000

    100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,27 +1,14 @@
 import Car from './10-car.js';
 
 class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super();
-    if (typeof brand !== 'string') {
-      throw new TypeError('Brand must be a string');
+    constructor(...args) {
+        super(...args);
+        [, , , this._range] = args;
     }
-    if (typeof motor !== 'string') {
-      throw new TypeError('Motor must be a string');
-    }
-    if (typeof color !== 'string') {
-      throw new TypeError('Color must be a string');
-    }
-    if (typeof range !== 'string') {
-      throw new TypeError('Range must be a string');
-    }
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
-    this._range = range;
-  }
 
-  cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
-  }
+    cloneCar() {
+        return new Car(this._brand, this._motor, this._color);
+    }
 }
+
+export default EVCar;
