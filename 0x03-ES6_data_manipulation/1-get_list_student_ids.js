148d75b91d76b87d3500857905dec41396041ab2
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-16 08:08:12 +0000

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,8 @@
-// 1-get_list_student_ids.js
-
-function getListStudentIds(array) {
-    // Check if the argument is an array
-    if (!Array.isArray(array)) {
-        return [];
-    }
-
-    // Use the map function to extract ids from the array of objects
-    return array.map(student => student.id);
+function getListStudentIds(list) {
+  if (!Array.isArray(list)) {
+    return [];
+  }
+  return list.map(student => student.id);
 }
 
 export default getListStudentIds;
