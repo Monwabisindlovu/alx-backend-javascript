6abde49bc09a3ed10bcde04bf80a1cc90f412440
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-16 09:37:11 +0000

    function tracks the number of times it is called with a particular endpoint. If the count reaches 5 or more, it throws an error with the message “Endpoint load is high

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,15 @@
 // 100-weak.js
-const weakMap = new WeakMap();
+export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  let count = weakMap.get(endpoint) || 0;
-  weakMap.set(endpoint, ++count);
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const count = weakMap.get(endpoint);
+    weakMap.set(endpoint, count + 1);
 
-  if (count >= 5) {
-    throw new Error('Endpoint load is high');
+    if (count + 1 >= 5) {
+      throw new Error('Endpoint load is high');
+    }
   }
 }
-
-export { weakMap };
