127f7daf96aee7a5229c43407503784aacbdd861
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-16 08:13:19 +0000

    1-main.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,5 @@
-function getListStudentIds(list) {
-  if (!Array.isArray(list)) {
-    return [];
-  }
-  return list.map(student => student.id);
-}
+// 1-get_list_student_ids.js
+
+const getListStudentIds = array => Array.isArray(array) ? array.map(student => student.id) : [];
 
 export default getListStudentIds;
