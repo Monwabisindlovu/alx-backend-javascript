209dcd4376fd8e8294d6739538b9fb593b253e51
monwabisi ndlovu <monwabisindlovu78@gmail.com>
2024-06-04 11:13:33 +0000

    index.js

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,22 +1,13 @@
-import express from 'express';
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-function controllerRouting(app) {
-  const router = express.Router();
-  app.use('/', router);
+const express = require('express');
 
-  router.get('/', (req, res) => {
-    AppController.getHomepage(req, res);
-  });
+const router = express.Router();
 
-  router.get('/students', (req, res) => {
-    StudentsController.getAllStudents(req, res, process.argv[2]);
-  });
+router.get('/', AppController.getHomepage);
+router.get('/students', StudentsController.getAllStudents);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
-  router.get('/students/:major', (req, res) => {
-    StudentsController.getAllStudentsByMajor(req, res, process.argv[2]);
-  });
-}
-
-module.exports = controllerRouting;
+module.exports = router;
+export default router;
