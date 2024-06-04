522513679985d2fbb6ee856fa345400d0835d9c0
monwabisi ndlovu <monwabisindlovu78@gmail.com>
2024-06-04 10:06:07 +0000

    7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,35 +1,29 @@
 const express = require('express');
-const fs = require('fs').promises;
-const app = express();
 
-const databasePath = process.argv[2];
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
+
+const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
+  const message = 'This is the list of our students\n';
   try {
-    const data = await fs.readFile(databasePath, 'utf8');
-    const lines = data.split('\n').filter((line) => line);
-    // Remove header
-    lines.shift();
-    const students = lines.map((line) => {
-      const [firstname, , , field] = line.split(',');
-      return { firstname, field };
-    });
-
-    const csStudents = students.filter((student) => student.field === 'CS').map((student) => student.firstname);
-    const sweStudents = students.filter((student) => student.field === 'SWE').map((student) => student.firstname);
-
-    res.send(`This is the list of our students\nNumber of students: ${students.length}\nNumber of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\nNumber of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-  } catch (err) {
-    res.status(500).send('Cannot load the database');
+    const students = await countStudents(DATABASE);
+    res.end(`${message}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${message}${error.message}`);
   }
 });
 
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
+app.listen(port, () => {
+  console.log(`Express Server running at http://localhost:${port}/`);
 });
 
 module.exports = app;
